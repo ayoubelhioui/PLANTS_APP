a093ceb93a6149a3f2db696d4a73f38343e524d0
Ayoub Elhioui <Elhioui001@gmail.com>
2022-07-12 14:56:07 -0700

    creating home page for this small app

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class PlantApp extends StatelessWidget {
+  const PlantApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 230, 255, 224),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/home_page_picture.png'),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Palants ',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Fresher',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
