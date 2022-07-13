f7940ef815bd499b4136c52db9ce02bf88be89c3
Ayoub Elhioui <Elhioui001@gmail.com>
2022-07-13 16:51:19 -0700

    Login Page UI almost done

--- lib/LoginForm.dart
+++ lib/LoginForm.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
+class RememberMeCheckBox extends StatefulWidget {
+  const RememberMeCheckBox({Key? key}) : super(key: key);
+
+  @override
+  State<RememberMeCheckBox> createState() => _RememberMeCheckBoxState();
+}
+
+class _RememberMeCheckBoxState extends State<RememberMeCheckBox> {
+  bool checkboxValue = false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Checkbox(
+        shape: const CircleBorder(),
+        value: checkboxValue,
+        activeColor: Colors.green,
+        onChanged: (value) {
+          if (value == null) return;
+          setState(() {
+            checkboxValue = value;
+          });
+        },
+      ),
+      const Text(
+        'Remember me',
+        style: TextStyle(
+          color: Colors.green,
+          fontWeight: FontWeight.bold,
+          fontSize: 11,
+        ),
+      )
+    ]);
+  }
+}
+
 class LoginForm extends StatelessWidget {
   const LoginForm({Key? key}) : super(key: key);
 
@@ -7,15 +42,72 @@ class LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     final _formKey = GlobalKey<FormState>();
     return Container(
-      width: 270,
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      // width: 270,
       child: Form(
           child: Column(
         children: [
           TextFormField(
+            autofocus: true,
             decoration: const InputDecoration(
-              labelText: 'Full Name',
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              filled: true,
+              hintText: 'Full Name',
+              prefixIcon: Icon(
+                Icons.person_outline,
+                color: Colors.green,
+              ),
             ),
           ),
+          const SizedBox(height: 20),
+          TextFormField(
+            autofocus: true,
+            decoration: const InputDecoration(
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              filled: true,
+              hintText: 'Password',
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.green,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const RememberMeCheckBox(),
+              InkWell(
+                onTap: () {},
+                child: const Text(
+                  'Forget Password?',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 11,
+                  ),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 40,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text('Login'),
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+              ),
+            ),
+          )
         ],
       )),
     );
--- lib/screens/Login_Page.dart
+++ lib/screens/Login_Page.dart
@@ -11,40 +11,51 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
         body: Column(
-      children: [
-        SizedBox(
-          child: Image.asset('assets/images/login_page.png'),
-          height: MediaQuery.of(context).size.height / 2.2,
-          // width: /*MediaQuery.of(context).size.width*/ double.infinity,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'Welcome ',
-              style: TextStyle(fontFamily: 'Breadbary', fontSize: 40),
+          children: [
+            SizedBox(
+              child: Image.asset('assets/images/login_page.png'),
+              height: MediaQuery.of(context).size.height / 2.2,
+              // width: /*MediaQuery.of(context).size.width*/ double.infinity,
             ),
-            Text(
-              'Plants',
-              style: TextStyle(
-                  fontFamily: 'Breadbary', fontSize: 40, color: Colors.green),
+            const SizedBox(
+              height: 20,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Welcome ',
+                  style: TextStyle(
+                    fontFamily: 'Breadbary',
+                    fontSize: 40,
+                  ),
+                ),
+                Text(
+                  'Plants',
+                  style: TextStyle(
+                    fontFamily: 'Breadbary',
+                    fontSize: 40,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            const Text(
+              'login to your account',
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const LoginForm(),
           ],
         ),
-        const Text(
-          'login to your account',
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const LoginForm(),
-      ],
-    ));
+      ),
+    );
   }
 }
